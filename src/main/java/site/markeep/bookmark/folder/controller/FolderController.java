d9406af9b664349675fc423b30a3f2f1fb19f924
hyejin <hyejinkim1219@naver.com>
2023-12-19 18:10:56 +0900

    POST-/folders/pin커뮤니티 폴더 핀 클릭시 마이페이지에 폴더 생성

--- src/main/java/site/markeep/bookmark/folder/controller/FolderController.java
+++ src/main/java/site/markeep/bookmark/folder/controller/FolderController.java
@@ -15,6 +15,7 @@ import site.markeep.bookmark.folder.dto.response.FolderResponseDTO;
 import site.markeep.bookmark.folder.service.FolderService;
 import site.markeep.bookmark.util.dto.page.PageDTO;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -32,7 +33,11 @@ public class FolderController {
         return ResponseEntity.ok().body(folderList);
     }
 
-    //폴더 추가 요청
+    /*****************************************************
+      .폴더 추가 요청
+      .생성 ENTITY : Folder , Tag , Folder image
+      .return : 정상 등록 유무 message
+    *****************************************************/
     @PostMapping("/my" )
     public  ResponseEntity<?> addFolder(
             @AuthenticationPrincipal TokenUserInfo userInfo,
@@ -40,9 +45,6 @@ public class FolderController {
             @RequestPart(value = "folderImage", required = false) MultipartFile folderImg,
             BindingResult result
     ){
-        log.info("/folders/my/ POST userInfo ! " +  userInfo);
-        log.info("/folders/my/ POST dto ! " +  dto);
-        log.info("/folders/my/ POST folderImg ! " +  folderImg);
 
         if(result.hasErrors()){
             log.warn(result.toString());
@@ -59,7 +61,7 @@ public class FolderController {
             if(folderImg != null) {
                 log.info("attached file name: {}", folderImg.getOriginalFilename());
                 // 전달받은 프로필 이미지를 먼저 지정된 경로에 저장한 후 DB 저장을 위해 경로를 받아오자.
-                uploadedFilePath = folderService.uploadProfileImage(folderImg);
+                uploadedFilePath = folderService.uploadFolderImage(folderImg);
             }
             folderService.addFolder(dto,userInfo.getId(), uploadedFilePath );
             return ResponseEntity.ok().body("정상 등록 되었습니다.");
@@ -69,12 +71,16 @@ public class FolderController {
         }
     }
 
-
-
-    @GetMapping("/all")
+    /*****************************************************
+     . 모든 폴더 LIST 를 조회 (SORT 순서: PIN COUNT DESC , CREATEDATE DESC)
+     . paging 처리
+     . return 조회건수 + page 객체 + Floder / Tag / Pin
+     *****************************************************/
+     @GetMapping("/all")
     public ResponseEntity<?> getFolderAllList(
 //            @AuthenticationPrincipal TokenUserInfo userInfo,
-            @Validated @RequestBody PageDTO dto,BindingResult result) {
+             PageDTO dto) {
+
         try {
             FolderListResponseDTO list = folderService.getList(dto);
             return ResponseEntity.ok().body(list);
@@ -86,5 +92,39 @@ public class FolderController {
         }
 
     }
+
+    /*****************************************************
+     . 커뮤니티 화면에서 폴더의 pin hover 누르면, 해당 폴더가 마이페이지에 생긴다.
+     . 1.userInfo 로 유저 check -> 없으면 "미가입 user 입니다. MSG"
+     . 2.(service) 해당 폴더가 있는지 check -> 없으면  "잘못된 폴더 번호 입니다. MSG"
+     . 3.(service) Folder , Site , Pin, FolderImg 생성
+     . 4.return Folder + Site , 정상 생성 유무
+     *****************************************************/
+    @PostMapping("/pin" )
+    public  ResponseEntity<?> addFolderPin(
+            @AuthenticationPrincipal TokenUserInfo userInfo,
+            int folderId
+    ) throws Exception {
+        log.info("/folders/pin/ POST userInfo ! " +  userInfo);
+        log.info("/folders/pin/ POST folderId ! " +  folderId);
+
+
+        if(userInfo == null || userInfo.getId() == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("미가입 회원입니다.");
+        }
+        try {
+            FolderResponseDTO folderResponseDTO =  folderService.addFolderPin(userInfo.getId(), Long.valueOf(folderId));
+            return  ResponseEntity.ok().body(folderResponseDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+    }
+
+
+
 
 }
